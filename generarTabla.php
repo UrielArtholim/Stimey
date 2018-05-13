ea5d53d68c47221787cb513b57ee2e5c11fb272a
UrielKaralden <urielkaralden@gmail.com>
2018-05-13 22:00:33 +0200

    Get table info to show it on a HTML Table by knowing the option chosen by the user

---
+++ generarTabla.php
@@ -0,0 +1,39 @@
+<?php
+  /*
+    PHP File to generate dynamically one HTML table
+    Used for showing the last 20 modifications
+  */
+
+    // Establecemos conexión con la base de datos
+    if(!isset($_SESSION)){session_start();}
+
+    $tabla = $_POST['origen']
+
+  	$user_id = $_SESSION['id'];
+    $conexion = mysqli_connect ("localhost", "root", "", "Stimey")
+    			or die("Error al conectar a la base de datos");
+
+
+    $post_header = "<html>
+      <head>
+        <title>
+          POST Request
+		    </title>
+		    <meta charset=\"UTF-8\">
+	   </head>
+     ";
+
+     $post_body = "
+     <body>
+        <form method = \"POST\" action = \"generarHTML.php\">
+            <input type = \"hidden\" name = \"tabla\" value = \"$tabla\">
+        </form>
+    </body>
+      ";
+
+      $post_footer = "</html>";
+
+      $post_request = $post_header.$post_body.$post_footer;
+      echo $post_request;
+      header("Location: generarHTML.php");
+?>
